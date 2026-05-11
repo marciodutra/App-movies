493af70887e0f34783436b5e86f96efeaeb28715
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-11 17:42:49 -0300

    Primeiro commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <h2>DutraFlix &copy; Desenvolvido por Márcio Dutra 2026</h2>
+        </footer>
+    );
+}
+
+export default Footer;
