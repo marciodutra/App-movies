96ceec02b61ffd0294dda70bcb4236ebb1311449
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-12 13:14:13 -0300

    Atualizações do projeto

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category, { categories, filterCategory } from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
 
 function App() {
   return (
     <>
       <Header />
       <Banner image="favoritos" />
-      <Container>
-        <h2>Geografia</h2>
-        <section className="cards">          
-          {videos.map((video) => <Card id={video.id} key={video.id} />)} 
-        </section>        
+      <Container>       
+
+        {
+          categories.map((category, index) => 
+            <Category category={category}>
+                {filterCategory(index).map((video) => <Card id={video.id} key={video.id} />)}
+            </Category>   
+          )
+        }    
+        
       </Container>
       <Footer />
     </>
---
+++ src/components/Category/index.js
@@ -0,0 +1,27 @@
+import styles from "./Category.module.css";
+import videos from "../../json/videos.json";
+
+export const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia"
+]
+
+export function filterCategory(id){
+  return videos.filter( video => video.category === categories[id])
+}
+
+export function Category({ category, children }){
+    return(
+        <section className={styles.category}>
+            <h2>{category}</h2>    
+            <div>          
+                { children }
+            </div>  
+        </section>
+    );
+}
+
+export default Category;
