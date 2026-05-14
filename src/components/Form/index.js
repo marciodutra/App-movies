f85c82ecc8e6ef295a8847e4bc564bafc636b59e
Marcio Dutra <professormarciodutra@gmail.com>
2026-05-14 13:22:41 -0300

    Atualizações do projeto

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,21 +4,46 @@ import styles from "./Form.module.css";
 
 function Form() {
 
-    const [url, setUrl ] = useState('')
-    const [category, setCategory ] = useState('')
+    const [url, setUrl] = useState('')
+    const [category, setCategory] = useState('')
     const [videos, setVideos] = useState([])
     const [errors, setErrors] = useState('')
+
+    function valideUrl(url) {
+        const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([a-zA-Z0-9\-_]+)$/
+
+        if (!regex.test(url) || url.length < 43) {
+            setErrors('ERRO: URL inválida!')
+            return false
+        } else {
+            return url.substring(32, 43)
+        }
+    }
+
 
     function onSave(e) {
         e.preventDefault()
         console.log(url, category)
+        if (!category || category === '-') {
+            setErrors('ERRO: Escolha uma categoria!')
+            return
+        } else {
+            setErrors('')
+        }
 
-        const newVideo = {url, category}
-        setVideos([...videos, newVideo])
-        console.log(videos)
+        const urlVideo = valideUrl(url)
+        if (urlVideo && category) {
+            const newVideo = { url, category }
+            setVideos([...videos, newVideo])
+            localStorage.setItem('videos', JSON.stringify([...videos, newVideo]))
+            setUrl('')
+            setCategory('')
+        }else{
+            setErrors('ERRO: Url inválida!')
+        }        
     }
 
-    return(
+    return (
         <section className={styles.container}>
             <h2>Cadatro de vídeos</h2>
             <form onSubmit={onSave}>
@@ -29,7 +54,9 @@ function Form() {
                         placeholder="Coloque a URL do vídeo"
                         required="required"
                         value={url}
-                        onChange={ e => setUrl(e.target.value) }
+                        onChange={e => setUrl(e.target.value)}
+                        minLength="43"
+                        maxLength="43"
                     />
                 </div>
                 <div>
@@ -37,7 +64,7 @@ function Form() {
                     <select
                         required="required"
                         value={category}
-                        onChange={ e => setCategory(e.target.value) }
+                        onChange={e => setCategory(e.target.value)}
                     >
                         <option value="-">Selecione uma categoria</option>
                         {categories.map(item => {
@@ -47,6 +74,9 @@ function Form() {
                 </div>
                 <div>
                     <button>Cadastrar</button>
+                </div>
+                <div>
+                    {errors}
                 </div>
             </form>
         </section>
